427dbc288b07da1355af348ab7bf739414e45f5a
gaetano lenoci <gaetano.lenoci@gmail.com>
2022-10-17 09:49:15 +0200

    wip

---
+++ CleanArchitecture.Functional.Tests/Projects/GetAll.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchitecture.Common.Tests.DatabaseSnapshot;
+using CleanArchitecture.Contracts.Projects;
+using CleanArchitecture.Domain.Aggregates;
+using CleanArchitecture.Functional.Tests.Common;
+using CleanArchitecture.WebApi;
+using Xunit;
+
+namespace CleanArchitecture.Functional.Tests.Projects;
+public class GetAll {
+    [Fact( DisplayName = "Get all projects, must return 3 projects." )]
+    public async Task _GetProjectById_MustReturnOk() {
+        var server = MockServerFactory.GetFakeServer();
+
+        Project project;
+
+        MockServerFactory
+            .DbSnapshoter
+            .AddProject( out project, "new project 1" )
+            .AddProject( out project, "new project 2" )
+            .AddProject( out project, "new project 3" );
+
+        var getProjectApiResult = await ApiRequester
+            .GetAsync<ProjectsDto>(
+                server, $"{Route.Project}" );
+
+        Assert.Equal( 3, getProjectApiResult.Value.Projects.Count() );
+    }
+}
